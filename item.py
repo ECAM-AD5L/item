d822cdb75ec894188312b8e345f1c4e3de27a606
T-Emine <eminosum@hotmail.com>
2018-12-17 15:02:07 +0100

    Item api rest

--- item.py
+++ item.py
@@ -1,63 +1,175 @@
-from flask import Flask
-from flask_restful import Api, Resource, reqparse
-app = Flask(__name__)
-api = Api(app)
+import os
+from pymongo import MongoClient
+import json
+from flask import request
 
-class Item():
-    def __init__(self, id, nom, prix, commentaire, description):
-        self.id = id
-        self.nom = nom
-        self.prix = prix
-        self.commentaire = commentaire
-        self.description = description
-
-    def get_nom(self):
-        return self.nom
-
-    def get_prix(self):
-        return self.prix
-
-    def get_id(self):
-        return self.id
-
-    def get_commentaire(self):
-        return self.commentaire
-
-    def get_description(self):
-        return self.description
-
-    def GET(self, name):
-        pass
-
-    def POST(self):
-        pass
-
-    def PUT(self):
-        pass
-
-    def DELETE(self):
-        pass
+client = MongoClient('mongodb://admin:password1@ds117334.mlab.com:17334/item')
+db = client.item
+collection = db.items
+print(collection)
 
 
-class Commentaire():
-    def __init__(self, note, texte):
-        self.note = note
-        self.texte = texte
+def get():
+    not_f = "Not Found"
+    search = collection.find()
+    dico = {}
 
-    def get_note(self):
-        return self.note
+    i=0
+    for s in search:
+        print(s)
+        dico[i] = json.dumps(str(s))
+        i+=1
 
-    def get_texte(self):
-        return self.texte
+    return (dico)
+
+def get_i(id):
+    search = collection.find_one({'id':id})
+    print("-----------------")
+    print(search)
+    dico = {}
+
+    if search != None:
+        return json.dumps(str(search))
+    else:
+        return "Not deleted"
+
+def delete(id):
+    search = collection.find_one({'id':id})
+    print("-----------------")
+    print(search)
+    dico = {}
+
+    if search != None:
+        collection.remove(search)
+        return "deleted"
+    else:
+        return "Not deleted"
 
 
-class Description():
-    def __init__(self, couleur, modele):
-        self.couleur = couleur
-        self.modele = modele
+def insert():
+    r = request.form
+    id = r["id"]
+    name = r["name"]
+    price = r["price"]
+    color = r["color"]
+    model = r["model"]
+    if (id=="") or (name=="") or (price=="") or (color=="") or (model==""):
+        return "Not inserted"
+    else:
+        item={
+            "id": id,
+            "name": name,
+            "price": price,
+            "description": {
+                "color": color,
+                "model": model
+            }
+        }
 
-    def get_couleur(self):
-        return self.couleur
+        rec = collection.insert_one(item)
+        if rec != None:
+            return "Inserted"
+        else:
+            return "Not inserted"
+        # print("Done", rec)
 
-    def get_modele(self):
-        return self.modele
+
+def insert_():
+        #item example for test
+        item = {
+            "id":"2",
+            "name": "Samsung S9",
+            "price": "800€",
+            "description": {
+                "color": "black",
+                "model": "64GB"
+            }
+        }
+
+        rec = collection.insert_one(item)
+        print("Done", rec)
+
+
+if __name__ == '__main__':
+    insert_()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# class Item():
+#     def __init__(self, id, nom, prix, commentaire, description):
+#         self.id = id
+#         self.nom = nom
+#         self.prix = prix
+#         self.commentaire = commentaire
+#         self.description = description
+#
+#     def get_nom(self):
+#         return self.nom
+#
+#     def get_prix(self):
+#         return self.prix
+#
+#     def get_id(self):
+#         return self.id
+#
+#     def get_commentaire(self):
+#         return self.commentaire
+#
+#     def get_description(self):
+#         return self.description
+#
+#     def GET(self, name):
+#         pass
+#
+#     def POST(self):
+#         pass
+#
+#     def PUT(self):
+#         pass
+#
+#     def DELETE(self):
+#         pass
+#
+#
+# class Commentaire():
+#     def __init__(self, note, texte):
+#         self.note = note
+#         self.texte = texte
+#
+#     def get_note(self):
+#         return self.note
+#
+#     def get_texte(self):
+#         return self.texte
+#
+#
+# class Description():
+#     def __init__(self, couleur, modele):
+#         self.couleur = couleur
+#         self.modele = modele
+#
+#     def get_couleur(self):
+#         return self.couleur
+#
+#     def get_modele(self):
+#         return self.modele
